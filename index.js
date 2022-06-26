4f58a2a4d292e62c6559d10385151ebc6f0d61e6
Yusuke Sato ( YS office LLC Founder ) <yusuke.sato@ys-office.llc>
2022-06-26 16:27:07 +0900

    ログ出力できるよう改修

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 const port = 3000;
+
+app.use(express.json());
+app.use(morgan("common"));
 
 app.get("/", (req, res) => {
   res.json({
