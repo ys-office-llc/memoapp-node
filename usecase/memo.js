72c59f75043f6431ff218d6ad985cf70afe39bcd
Yusuke Sato ( YS office LLC Founder ) <yusuke.sato@ys-office.llc>
2022-06-26 16:45:14 +0900

    サービス層からデータを取得できるよう修正

--- usecase/memo.js
+++ usecase/memo.js
@@ -1,20 +1,4 @@
 exports.getAll = () => {
-  const memos = [
-    {
-      _id: "617c9c32c5c70e85e21668a1",
-      message: "顔洗う",
-      checked: false,
-    },
-    {
-      _id: "617c9c38c5c70e85e21668a4",
-      message: "歯磨きする",
-      checked: false,
-    },
-    {
-      _id: "617c9c48c5c70e85e21668a7",
-      message: "朝ごはんを食べる",
-      checked: true,
-    },
-  ];
-  return memos;
+  // TODO: service層からデータを取得できるようにする
+  return [];
 };
