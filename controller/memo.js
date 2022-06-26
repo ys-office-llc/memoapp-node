7c13f798982c2f9e579ffee3618d07eb0c388df8
Yusuke Sato ( YS office LLC Founder ) <yusuke.sato@ys-office.llc>
2022-06-26 16:43:54 +0900

    ユースケース層を実装

--- controller/memo.js
+++ controller/memo.js
@@ -1,21 +1,7 @@
+const usecase = require("../usecase");
+
 exports.getAll = (req, res, next) => {
   console.log(req, next);
-  const memos = [
-    {
-      _id: "617c9c32c5c70e85e21668a1",
-      message: "顔洗う",
-      checked: false,
-    },
-    {
-      _id: "617c9c38c5c70e85e21668a4",
-      message: "歯磨きする",
-      checked: false,
-    },
-    {
-      _id: "617c9c48c5c70e85e21668a7",
-      message: "朝ごはんを食べる",
-      checked: true,
-    },
-  ];
+  const memos = usecase.memo.getAll();
   return res.status(200).send({ memos: memos });
 };
