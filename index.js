a34e57b5df31eb08166b4a9b43439c5af7996a81
Yusuke Sato ( YS office LLC Founder ) <yusuke.sato@ys-office.llc>
2022-06-26 18:26:58 +0900

    フロントエンド実装（CORS 含む）

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
 const port = 3000;
 const controller = require("./controller");
 
+app.use(cors());
 app.use(express.json());
 app.use(morgan("common"));
 mongoose.connect("mongodb://localhost:27017/memoApp", { useNewUrlParser: true, useUnifiedTopology: true });
