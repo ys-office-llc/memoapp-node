2c09565c87d5507dc2d49fdeb3345e54dbd86d29
Yusuke Sato ( YS office LLC Founder ) <yusuke.sato@ys-office.llc>
2022-06-26 16:21:31 +0900

    ダブルクォートに統一した

--- .prettierrc.js
+++ .prettierrc.js
@@ -4,6 +4,6 @@ module.exports = {
   trailingComma: 'es5', // 複数行の場合、可能な限り末尾のコンマを出力
   tabWidth: 2, // タブサイズ
   semi: true, // ステートメントの最後にセミコロンを付与
-  singleQuote: true, // 二重引用符の代わりに単一引用符を使用
+  singleQuote: false, // 二重引用符の代わりに単一引用符を使用
   endOfLine: 'auto', // 改行の文字コードを指定
 };
